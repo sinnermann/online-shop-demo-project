bc1214c42b5c62ed9ffd1bd217670f9b8c03350c
sinnermann <ealijev@gmail.com>
2025-09-15 15:25:14 +0400

    Second commit cart items adding

--- src/Components/CartDropdown/CartDropdown.jsx
+++ src/Components/CartDropdown/CartDropdown.jsx
@@ -1,12 +1,19 @@
 import "./CartDropdown.styles.scss";
 import Button from "../Button/Button";
+import CartItem from "../CartItem/CartItem";
+import { useContext } from "react";
+import { CartContext } from "../../Context/CartContext";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items" />
-      <Button>Go to Checkout</Button>
-
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+        <Button>Go to Checkout</Button>
+      </div>
     </div>
   );
 };
--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,16 +1,24 @@
-import './ProductCard.scss';
-import Button from '../Button/Button';
+import "./ProductCard.scss";
+import Button from "../Button/Button";
+import { useContext } from "react";
+import { CartContext } from "../../Context/CartContext";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+  const addProductToCart = () => addItemToCart(product)
+
+
   return (
-    <div className='product-card-container'>
+    <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
-      <div className='footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
+      <div className="footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
       </div>
-      <Button buttonType='inverted'>Add to card</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add to card
+      </Button>
     </div>
   );
 };
