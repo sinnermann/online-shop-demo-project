2ef1fddfc1d6389cc457b257885cddc42cd5c957
sinnermann <ealijev@gmail.com>
2025-09-20 13:44:50 +0400

    Styles corrections

--- src/Components/Category-Item/CategoryItem.component.jsx
+++ src/Components/Category-Item/CategoryItem.component.jsx
@@ -1,14 +1,19 @@
 import "./Category-item.styles.scss";
+import { useNavigate } from "react-router-dom";
 
 const CategoryItem = ({ category }) => {
-  const {imageUrl, title} = category;
+  const { imageUrl, title } = category;
+  const navigate = useNavigate();
   return (
     <div className="category-container">
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
       ></div>
-      <div className="category-body-container">
+      <div
+        className="category-body-container"
+        onClick={() => navigate("/shop")}
+      >
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
--- src/Routs/Shop/Trends.jsx
+++ src/Routs/Shop/Trends.jsx
@@ -1,52 +1,54 @@
-
 import "./Shop.scss";
+import { useNavigate } from "react-router-dom";
 
 const Trends = () => {
   const categories = [
     {
       id: 1,
-      title: "hats",
+      title: "HATS",
       imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
       id: 2,
-      title: "jackets",
+      title: "JACKETS",
       imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
       id: 3,
-      title: "sneakers",
+      title: "SNEAKERS",
       imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
       id: 4,
-      title: "sweaters",
+      title: "SWEATERS",
       imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
-      title: "mens",
+      title: "MENS",
       imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
     {
       id: 6,
-      title: "coats",
+      title: "COATS",
       imageUrl:
         "https://www.politix.com.au/on/demandware.static/-/Library-Sites-PolitixSharedLibrary/default/dwa3a520d7/images/Blog/2022/05%20-%20May/ultimate-guide-to-winter-jackets/PLTX245_Winter_Jackets_Blog_Header_Desktop.jpg",
     },
     {
       id: 7,
-      title: "womens",
+      title: "WOMENS",
       imageUrl:
         "https://fabriclore.com/cdn/shop/articles/Women_s_jackets_and_types.png?v=1746596114&width=1000",
     },
     {
       id: 8,
-      title: "watches",
+      title: "WATCHES",
       imageUrl:
         "https://shop.timexindia.com/cdn/shop/articles/2.png?v=1718351785",
     },
   ];
+
+  const navigate = useNavigate();
 
   return (
     <div className="directory-container">
@@ -57,7 +59,10 @@ const Trends = () => {
             className="background-image"
             style={{ backgroundImage: `url(${category.imageUrl})` }}
           ></div>
-          <div className="category-body-container">
+          <div
+            className="category-body-container"
+            onClick={() => navigate("/shop")}
+          >
             <h2>{category.title}</h2>
             <p>Shop Now</p>
           </div>
